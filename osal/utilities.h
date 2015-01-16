0273cce8a96b0ea54e75937209e3e16264f095b6
Sibin P Thomas <sibinpthomas@gmail.com>
2015-01-16 13:13:08 +0530

    Added macro which gives the length of the array irrespective of array element type.

--- osal/utilities.h
+++ osal/utilities.h
@@ -16,6 +16,9 @@ extern "C"
 #define SET_BITNUM(val, bitnum)             ((val) |= (1 << (bitnum)))
 #define CLR_BITNUM(val, bitnum)             ((val) &= (~(1 << (bitnum))))
 
+/* Macro to calculate the length of arrays irrespective of their types. */
+#define LEN_OF_ARRAY(array)                 (sizeof((array))/sizeof((array)[0]))
+
 /* Compile time assert declaration. */
 #define CT_ASSERT_ON(condition)             extern void ct_assert(char [sizeof(char[1 - 2*!!(condition)])])
 /* Compile time assert statement. */
