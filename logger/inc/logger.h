da8d42b6a75431a80b68a8c2c5bcb616e255e5c5
Sibin P Thomas <sibinpthomas@gmail.com>
2016-04-18 20:41:09 +0530

    Merged Loggger changes from Soft LL. (Log_Temp())
    * Also removed defines of LOGGER and LOGGER_LEVEL since the vimrc Make
        commands provides these.
        * Vimrc commit #431b1b35cce31b73e9d3d7148cee2d88a68e1a4b.

--- logger/inc/logger.h
+++ logger/inc/logger.h
@@ -6,11 +6,6 @@ extern "C"
 {
 #endif /* __cplusplus */
 
-#define LOGGER
-#ifndef LOGGER_LEVEL
-    #define LOGGER_LEVEL                        0    
-#endif /* LOGGER_LEVEL */
-
 #include "logger_internal.h"
 
 
@@ -20,20 +15,35 @@ extern "C"
     API_RESULT init_logger(void);
     void shutdown_logger(void);
     
-    #if defined(LOGGER_LEVEL) && (LOGGER_LEVEL >= 0) && (LOGGER_LEVEL <= 5)
+    #if defined(LOGGER_LEVEL) && (LOGGER_LEVEL >= 0) && (LOGGER_LEVEL <= 6)
+
+        #if LOGGER_LEVEL <= 6
+        /*
+         * This level of logging is to be used for short-term testing only, 
+         * usually the unit-testing phase.
+         * Ideally these logging APIs should not be found in checked-in code.
+         */
+            #define log_temp(msg) fprintf (log_f_hdl, "\n[!! TEMP !!]:[%s]:[%d]: ", __FILE__, __LINE__); \
+                                  _log msg;
+            #define _log_temp(stamp, msg) fprintf (log_f_hdl, "\n[!! TEMP !!]:[%s]:[%d]:" stamp " ", __FILE__, __LINE__); \
+                                          _log msg;
+
+            #define log_simple_temp(msg) _log msg;
+            #define log_buffer_temp(buf, buf_len) _hex_dump((buf), (buf_len));
+        #endif /* LOGGER_LEVEL > 5 */
 
         #if LOGGER_LEVEL <= 5
             #define log_critical(msg) fprintf (log_f_hdl, "\n[!! CTCL !!]:[%s]:[%d]: ", __FILE__, __LINE__); \
-                                     _log msg;
+                                      _log msg;
             #define _log_critical(stamp, msg) fprintf (log_f_hdl, "\n[!! CTCL !!]:[%s]:[%d]:" stamp " ", __FILE__, __LINE__); \
-                                             _log msg;
+                                              _log msg;
         #endif /* LOGGER_LEVEL > 4 */
 
         #if LOGGER_LEVEL <= 4
             #define log_error(msg) fprintf (log_f_hdl, "\n[** ERR **]:[%s]:[%d]: ", __FILE__, __LINE__); \
-                                     _log msg;
+                                   _log msg;
             #define _log_error(stamp, msg) fprintf (log_f_hdl, "\n[** ERR **]:[%s]:[%d]:" stamp " ", __FILE__, __LINE__); \
-                                             _log msg;
+                                           _log msg;
         #endif /* LOGGER_LEVEL > 3 */
 
         #if LOGGER_LEVEL <= 3
@@ -45,26 +55,26 @@ extern "C"
 
         #if LOGGER_LEVEL <= 2
             #define log_info(msg)  fprintf (log_f_hdl, "\n[== INF ==]:[%s]:[%d]: ", __FILE__, __LINE__); \
-                                     _log msg;
+                                   _log msg;
             #define _log_info(stamp, msg)  fprintf (log_f_hdl, "\n[== INF ==]:[%s]:[%d]:" stamp " ", __FILE__, __LINE__); \
-                                             _log msg;
+                                           _log msg;
         #endif /* LOGGER_LEVEL > 1 */
 
         #if LOGGER_LEVEL <= 1
             #define log_trace(msg) fprintf (log_f_hdl, "\n[-- TRC --]:[%s]:[%d]: ", __FILE__, __LINE__); \
-                                     _log msg;
+                                   _log msg;
             #define _log_trace(stamp, msg) fprintf (log_f_hdl, "\n[-- TRC --]:[%s]:[%d]:" stamp " ", __FILE__, __LINE__); \
-                                             _log msg;
+                                           _log msg;
 
             #define log_simple(msg) _log msg;
             #define log_buffer(buf, buf_len) _hex_dump((buf), (buf_len));
         #endif /* LOGGER_LEVEL > 0 */
 
-    #else /* defined(LOGGER_LEVEL) && (LOGGER_LEVEL >= 0) && (LOGGER_LEVEL <= 5) */
-        #if defined(LOGGER_LEVEL) && ((LOGGER_LEVEL < 0) || (LOGGER_LEVEL > 5))
-            #error "LOGGER_LEVEL should be a value in the range [0, 5]."
-        #endif /* defined(LOGGER_LEVEL) && ((LOGGER_LEVEL < 0) || (LOGGER_LEVEL > 5)) */
-    #endif /* defined(LOGGER_LEVEL) && (LOGGER_LEVEL >= 0) && (LOGGER_LEVEL <= 5) */
+    #else /* defined(LOGGER_LEVEL) && (LOGGER_LEVEL >= 0) && (LOGGER_LEVEL <= 6) */
+        #if defined(LOGGER_LEVEL) && ((LOGGER_LEVEL < 0) || (LOGGER_LEVEL > 6))
+            #error "LOGGER_LEVEL should be a value in the range [0, 6]."
+        #endif /* defined(LOGGER_LEVEL) && ((LOGGER_LEVEL < 0) || (LOGGER_LEVEL > 6)) */
+    #endif /* defined(LOGGER_LEVEL) && (LOGGER_LEVEL >= 0) && (LOGGER_LEVEL <= 6) */
     
 #else /* LOGGER */
     /* Always call init_logger() as part of a conditional statement otherwise
@@ -76,7 +86,7 @@ extern "C"
 
 #ifndef log_simple
     #define log_simple(msg)
-#endif /* log_buffer */
+#endif /* log_simple */
 
 #ifndef log_buffer
     #define log_buffer(buf, buf_len)
@@ -106,6 +116,19 @@ extern "C"
     #define log_critical(msg)
     #define _log_critical(stamp, msg)
 #endif /* log_critical */
+
+#ifndef log_temp
+    #define log_temp(msg)
+    #define _log_temp(stamp, msg)
+#endif /* log_temp */
+
+#ifndef log_simple_temp
+    #define log_simple_temp(msg)
+#endif /* log_simple_temp */
+
+#ifndef log_buffer_temp
+    #define log_buffer_temp(buf, buf_len)
+#endif /* log_buffer_temp */
 
 #ifdef __cplusplus
 }
